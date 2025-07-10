446e4e8ddb27cf3fcf2cb67837bc41970be30e2e
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-10 20:16:43 +0200

    Commited

--- app/src/main/java/com/example/trymenutoolbar3/MainActivity.java
+++ app/src/main/java/com/example/trymenutoolbar3/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.trymenutoolbar3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Button button;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textMain);
-
+        clickMenu(R.id.toolbarId);
 
     }
 
-    private void clickMenu() {
+    private void clickMenu(int id) {
+        imageButton = findViewById(id);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActitivity.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "test a", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
