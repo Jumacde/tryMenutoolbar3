ae01029a16420b2f684b065f00d36b2e2cb147fb
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-10 21:28:25 +0200

    Commited

--- app/src/main/java/com/example/trymenutoolbar3/MainActivity.java
+++ app/src/main/java/com/example/trymenutoolbar3/MainActivity.java
@@ -10,14 +10,13 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
-    private Button button;
     private ImageButton imageButton;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +24,63 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
         textView = findViewById(R.id.textMain);
-        clickMenu(R.id.menuIcon);
+        //clickMenu(R.id.menuIcon);
+        setUpMenuIcon();
+        setupDrawerBackIcon();
 
     }
 
-    private void clickMenu(int id) {
+
+    /*
+    *     private void clickMenu(int id) {
         imageButton = findViewById(id);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SubActitivity.class);
                 startActivity(intent);
-                Toast.makeText(MainActivity.this, "test a", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "test successfully", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    * */
 
+    private void setUpMenuIcon() {
+        imageButton = findViewById(R.id.menuIcon);
+        if ( imageButton != null) {
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawerLayout.openDrawer(GravityCompat.END);
+                    Toast.makeText(MainActivity.this, "drawer success", Toast.LENGTH_SHORT).show();
+                }
+            });
 
+        }
+    }
+
+    private void setupDrawerBackIcon() {
+        imageButton = findViewById(R.id.backIcon);
+        if (imageButton != null) {
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "close drawer", Toast.LENGTH_SHORT).show();
+                    drawerLayout.closeDrawer(GravityCompat.END);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
