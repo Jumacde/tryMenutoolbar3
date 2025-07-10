077246727dfcd9b6707cdfdee7fd50b3f3ffe1c2
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-10 20:25:00 +0200

    Commited

--- app/src/main/java/com/example/trymenutoolbar3/SubActitivity.java
+++ app/src/main/java/com/example/trymenutoolbar3/SubActitivity.java
@@ -1,11 +1,16 @@
 package com.example.trymenutoolbar3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SubActitivity extends AppCompatActivity {
+    private TextView textView;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,16 @@ public class SubActitivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.second_activity_main);
 
-    }
+        backPage();
 
+    }
+    private void backPage() {
+        imageButton = findViewById(R.id.backIcon);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
